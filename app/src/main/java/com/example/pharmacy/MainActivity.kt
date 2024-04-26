ff8fec916d6fa52bbfaf2ec8de2982be86d2fede
TerryTess <terrywamboi.ke@gmail.com>
2024-04-26 09:18:27 +0300

    initial commit

---
+++ app/src/main/java/com/example/pharmacy/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.pharmacy
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        // Set up the toolbar
+//        val toolbar: Toolbar = findViewById(R.id.toolbar)
+//        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false) // Hide the default title
+
+        // Set the logo in the toolbar
+//        toolbar.setLogo(R.drawable.img) // Replace "your_logo" with the name of your logo drawable
+//        toolbar.contentDescription = getString(R.string.app_name) // Optional: Add content description
+
+        // Apply insets to the main layout
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
